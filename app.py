8547821f64e20366246fd1e77fadd226207e7619
mr-bacco <mrbacco04@gmail.com>
2019-11-22 21:09:33 +0000

    commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 # Author: mrbacco                                      #
 # email: mrbacco@mrbacco.com                           #
 # Date: Q4/2019 - Q1/2020                              #
+# main file: app.py                                    #
 ########################################################
 
 from flask import Flask, render_template, url_for, session, request, redirect, logging, flash  # modules from flask for web server
@@ -10,16 +11,17 @@ from data import Items
 import pymongo # driver for mongdb connectivity
 import pandas as pd
 import scrapy as scrapy
-from bs4 import BeautifulSoup # used for web scraping
-import requests # the request HTTP library allows managing HTTP request/responses to/from websites
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators #FOR THE WEBFORMS
+#from flask_wtf import URLField
 from passlib.hash import sha512_crypt #passowrd hashing
 import logging
 from functools import wraps
 from emails import send_mail
 from flask_mail import Mail, Message
 from datetime import datetime
-
+from bs4 import BeautifulSoup
+import requests
+import pprint
 
 app = Flask(__name__) # creating an instalnce of the Flask class for thsi app as web server
 
@@ -33,7 +35,7 @@ app.config.update(dict(
     MAIL_USE_TLS = False,
     MAIL_USE_SSL = True,
     MAIL_USERNAME = 'campigotto111@gmail.com',
-    MAIL_PASSWORD = 'Daoxiao99'
+    MAIL_PASSWORD = 'Password2020!'
 ))
 
 mail = Mail(app)
@@ -41,27 +43,24 @@ mail = Mail(app)
 print(" connected to email ... probably")
 ############## email server SETUP END ##############
 
-
 ############## db SETUP START ##############
 # using mongo db cloud version
 # checking the connection to cloud ongodb and printing in the console the list of collections under the database
 
 try:
-    myclient = pymongo.MongoClient("mongodb://mrbacco:mongodb001@cluster0-shard-00-00-goutv.mongodb.net:27017,cluster0-shard-00-01-goutv.mongodb.net:27017,cluster0-shard-00-02-goutv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
-    mydb = myclient["lawyers"]
-    mycol = mydb["feedback"]
+    myclient = pymongo.MongoClient("mongodb://mrbacco:mongodb001@cluster0-shard-00-00-goutv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+    mydb = myclient["webscraping"] 
+    mycol = mydb["scraping"]
     print("if connected to db, then these are the collections in mydb: ", mydb.list_collection_names()) #used to check if db is connected
 except:
-    logging.warning("Could not connect to MongoDB")
+    logging.warning("not connected to mongodb")
 
 ############## db SETUP END ##############
 
 ############## defining the routes for the different web pages START ##############
 class Init(Form): #definition of a class for the init form
-    name = StringField('Name', [validators.Length(min = 1, max = 100)])
-    telefono = StringField('Telefono', [validators.Length(min = 5, max = 50)])
-    email = StringField('Email', [validators.Length(min = 6, max = 50)])
-    messaggio = StringField('Messaggio', [validators.DataRequired()])
+    url = StringField('URL', [validators.URL()])
+    email = StringField('Email', [validators.Email()])
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
@@ -70,24 +69,49 @@ def index():
         return render_template('home.html', form = form), logging.warning("you are under the home page now using GET, well done bacco ")
     if request.method == 'POST' and form.validate():
         # the following are the data from the init form
-        name = form.name.data 
-        telefono = form.telefono.data
+        url = form.url.data
         email = form.email.data
-        messaggio = form.messaggio.data
 
         # defining a new variable taking as input the values from the init form
         mymsg=[{
-                "name": name, 
-                "telefono": telefono, 
-                "email" : email, 
-                "messaggio" : messaggio
-                }]
+                "url": url,
+                "email" : email,
+              }]
         # insert the list into the mongo db
-        x = mycol.insert_many(mymsg), print("inserting this user: ", mymsg, "in the database called ", mycol)
-        msg = Message('New message from: ', sender='campigotto111@gmail.com', recipients=['mrbacco@mrbacco.com'], html = f"<h3> new message from: </h3> <ul><li>NOME: {name}</li> <li>TELEFONO: {telefono}</li><li> EMAIL: {email}</li> <li> MESSAGGIO: {messaggio}</li> <li> DATA e ORA: {readtime}</li>" )
-        mail.send(msg)
+        x = mycol.insert_many(mymsg), print("inserting this item: ", mymsg, "in the database called ", mycol)
+        # send an email to mrbacco@mrbacco.com for testing purposes: PLEASE DISABLE THIS IN PRODUCTION!!!!!
+        #msg = Message("NEW MESSAGE: ", sender='campigotto111@gmail.com', recipients=["mrbacco@mrbacco.com"], html = f"<h3> new message from: </h3> <ul><li>URL: {url}</li> <li> EMAIL: {email}</li> <li> DATA e ORA: {readtime}</li>")
+        #mail.send(msg)
+        # validating the url in the form http://...
+        #url1="http://"+url #making sure the parser will get the HTTP://WWW.EXAMPLE.COM syntax
         
-    return render_template('home.html', form = form), print("you are under the home page now using POST, data are sent to database")
+        result = requests.get(url) # getting the url from the webform
+        print("the requested url is: ", url) # printing the url to make sure the variable contains it
+        print("the response code is: ", result.status_code)            
+        #print(result.headers)
+        #now I can apply the BS4 class to the content of the page
+
+        payload = result.content # defining a new variable that takes the content of the web page
+        soup = BeautifulSoup(payload, "html.parser")
+        values=[]
+        for div in soup.find_all("div"): #looping to find all the "a" references
+            value = [a.text for a in soup.find_all("twitter")] #looping to find all the "a" references
+            values.append(value)
+
+        
+        pprint.pprint(values) #using .text allows to extract only the text on the webpage and not the tags
+         
+
+
+
+
+
+
+
+    return render_template('home.html', form = form), print("you are under the home page now, mrbacco ...")
+    
+
+
 
 ############## defining the routes for the different web pages END ##############
 
@@ -96,10 +120,8 @@ def index():
 
 
 
-
-
 ####################################################################################################
-# running app in debug mode so that I can update the app.py without the need of manual restart
+# running the app and enabling debug mode so that I can update the app.py without the need of manual restart
 if __name__ == "__main__":
     app.secret_key="mrbacco1974"
     app.run(debug=True)
